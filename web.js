9844b2aceeebe0a33e4c24bc51811e70dbf25881
Alexandre Guy <alexandre.guy@gmail.com>
2013-07-10 02:40:57 +0000

    my code

--- web.js
+++ web.js
@@ -3,8 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!2');
+    var fs = require('fs');
+
+    var buf = new Buffer (fs.readFileSync("index.html", "utf-8"));
+
+    response.send(buf.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
