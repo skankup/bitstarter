e2551ebc0c492c240ffd30266001b4317c304b18
Alexandre Guy <alexandre.guy@gmail.com>
2013-07-11 01:54:22 +0000

    finally found the bugs

--- grader.js
+++ grader.js
@@ -26,6 +26,11 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://rocky-beyond-5179.herokuapp.com";
+
+var sys = require('util'),
+    rest = require('restler');
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +70,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <url>', 'Web URL', rest.get('http://rocky-beyond-5179.herokuapp.com/').on('complete', function(result){
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+//  } else {
+//    sys.puts(result);
+  }
+}), URL_DEFAULT)
+  .parse(process.argv);
+  
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
